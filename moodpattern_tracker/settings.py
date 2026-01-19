92005018b7775c42b7310534653a5ebb65b8326b
LutiyKamen <vantikmsml@gmail.com>
2026-01-20 02:07:18 +0500

    quick fix settings.py

--- moodpattern_tracker/settings.py
+++ moodpattern_tracker/settings.py
@@ -9,6 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = 'False'
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 ALLOWED_HOSTS = [
     'moodpattern-tracker-production.up.railway.app',
     'localhost',
